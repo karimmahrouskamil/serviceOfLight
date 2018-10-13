7e0bdb4185f9f10652dacfb2097d60ecb67b29cd
karim kamil <kkamil@eg.ibm.com>
2018-10-13 14:39:27 +0200

    added search bar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,16 +8,40 @@ import { MakhdoumDetailsPage } from "../makhdoum-details/makhdoum-details";
   templateUrl: "home.html"
 })
 export class HomePage {
- 
+  _mahdouminService: any;
   makhdouminList: [MakhdominProvider];
+  _makhdouminList: any;
+  searchQuery: string = "";
+
   constructor(
     public navCtrl: NavController,
     mahdouminService: MakhdominServiceProvider
   ) {
-    this.makhdouminList = mahdouminService.getMakhdominData();
-    //console.log(this.makhdouminList);
+    this._mahdouminService = mahdouminService;
+    this.initializeData();
   }
+
+  initializeData() {
+    this.makhdouminList = this._mahdouminService.getMakhdominData();
+  }
+
   passDetailOfMakhdoum(oneMakhdoum): void {
     this.navCtrl.push(MakhdoumDetailsPage, oneMakhdoum);
+  }
+
+  getItems(ev: any) {
+    this.initializeData();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != "") {
+      this._makhdouminList = this.makhdouminList.filter(eachMakhdoum => {
+        return eachMakhdoum.name.toLowerCase().indexOf(val.toLowerCase()) > -1;
+      });
+
+      this.makhdouminList = this._makhdouminList;
+    }
   }
 }
