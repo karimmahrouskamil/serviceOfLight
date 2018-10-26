cd2c32dd30d8d972a66a4413647fcce684c34fcc
karim kamil <kkamil@eg.ibm.com>
2018-10-26 16:49:42 +0200

    read from json

--- src/providers/makhdomin-service/makhdomin-service.ts
+++ src/providers/makhdomin-service/makhdomin-service.ts
@@ -1,7 +1,9 @@
 // import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MakhdominProvider, ChamasLevel } from "../makhdomin/makhdomin";
-
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/timeout";
 /*
   Generated class for the MakhdominServiceProvider provider.
 
@@ -11,8 +13,8 @@ import { MakhdominProvider, ChamasLevel } from "../makhdomin/makhdomin";
 @Injectable()
 export class MakhdominServiceProvider {
   makhdouminList: [MakhdominProvider];
-
-  constructor() {
+  dataDummy : any;
+  constructor(public http: Http) {
     console.log("Hello MakhdominServiceProvider Provider");
   }
 
@@ -59,7 +61,15 @@ export class MakhdominServiceProvider {
       }
     ];
     this.makhdouminList = data;
-
+this.getMyData();
     return this.makhdouminList;
+  }
+  getMyData() {
+    this.http
+      .get("assets/data.json")
+      .map(res => res.json())
+      .subscribe(data => {
+        this.dataDummy = data;
+      });
   }
 }
