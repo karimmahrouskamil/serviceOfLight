63686aa475dde864d76d3086cfb2fc3beaefd9c1
karimMahrousKamil <karimmahrouskamil@hotmail.com>
2018-11-06 18:28:52 +0200

    quick change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,8 +25,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: "Home", component: HomePage },
-      { title: "Makhdoum-details-page", component: MakhdoumDetailsPage }
+      { title: "Home", component: HomePage }
+      // { title: "Makhdoum-details-page", component: MakhdoumDetailsPage }
     ];
   }
 
